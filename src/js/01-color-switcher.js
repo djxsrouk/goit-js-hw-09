a8de61ea46d1d1b9e965068a6302e381cc5d161e
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-02-18 18:43:47 +0000

    task 1 finish

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+
+let timer = null;
+
+startButton.addEventListener("click", () => {
+  startButton.setAttribute("disabled", true);
+  stopButton.removeAttribute("disabled");
+  timer = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+stopButton.addEventListener("click", () => {
+  startButton.removeAttribute("disabled");
+  stopButton.setAttribute("disabled", true); 
+  clearInterval(timer);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`; 
+};
